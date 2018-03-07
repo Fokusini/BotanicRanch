258386185ec5489b9b205f6aeb09c849d06fdd01
Albert Campillo Boixet <campillo.boixet.albert@gmail.com>
2018-03-07 17:22:38 +0100

    Inicio

---
+++ BotanicRanche/Assets/Scripts/InputManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class InputManager : MonoBehaviour
+{
+    //private LevelManager manager;
+    private PlayerBehaviour player;
+    public Transform canvasPause;
+    public GameObject canvasGameplay;
+
+    private AudioPlayer audioPlayer;
+
+    void Start()
+    {
+        AudioManager.Initialize();
+
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerBehaviour>();
+
+        audioPlayer = GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<AudioPlayer>();
+
+        //metalBox = GameObject.FindGameObjectWithTag("MetalBox").GetComponent<BoxBehaviour>();
+        //woodBox = GameObject.FindGameObjectWithTag("WoodBox").GetComponent<BoxBehaviour>();
+    }
+
+    void Update()
+    {
+        InputAxis();
+        InputJump();
+        InputRun();
+        InputAbility();
+        InputPause();
+        InputGod();
+        //InputLevelManager();
+    }
+    
+    void InputPause()
+    {
+        /*if(Input.GetButtonDown("Pause"))
+        {
+            Debug.Log("Pause");
+            // Pausar el juego
+            if(canvasPause.gameObject.activeInHierarchy == false)
+            {
+                //canvasPause.gameObject.SetActive(true);
+                //canvasGameplay.SetActive(false);
+                Time.timeScale = 0;
+                audioPlayer.StopMusic();
+                audioPlayer.PlayMusic(1);
+            }
+            else
+            {
+                //canvasPause.gameObject.SetActive(false);
+                //canvasGameplay.SetActive(true);
+                Time.timeScale = 1;
+                audioPlayer.StopMusic();
+                audioPlayer.PlayMusic(0);
+            }
+        }*/
+    }
+    void InputGod()
+    {
+        if(Input.GetKeyDown(KeyCode.F10))
+        {
+            Debug.Log("GOD");
+            player.SetGod();
+        }
+    }
+}
---
+++ BotanicRanche/Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneTransition : MonoBehaviour
+{
+    float currentTime;
+    float fadeTime = 2.0f;
+    int sceneToLoad = 0;
+    public Image blackScreen;
+
+    bool fadeOut = false;
+
+    void Start()
+    {
+        blackScreen.color = Color.black;
+        FadeIn();
+    }
+
+    void Update()
+    {
+        if(!fadeOut) return;
+
+        currentTime += Time.deltaTime;
+
+        if(currentTime >= fadeTime + .1f)
+        {
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+
+    public void LoadScene(string title)
+    {
+        SceneManager.LoadScene(title);
+    }
+
+    void FadeIn()
+    {
+        blackScreen.CrossFadeAlpha(0, fadeTime, true);
+    }
+    void FadeOut()
+    {
+        blackScreen.CrossFadeAlpha(1, fadeTime / 4, true);
+        fadeOut = true;
+    }
+
+    public void LoadScene(int n)
+    {
+        sceneToLoad = n;
+        FadeOut();
+    }
+}
